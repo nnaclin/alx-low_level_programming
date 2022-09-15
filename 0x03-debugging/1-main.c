b804a4030ab92248a87f63b85d2caa08d8e71fe7
Ikenna Nwodo <nnaclin@yahoo.com>
2022-09-15 09:33:18 -0700

    Task 0 Debuggging

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - this tests function that prints if integer is positve or negative
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+
+	return (0);
+
+
+}
