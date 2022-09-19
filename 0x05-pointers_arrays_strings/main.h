fae4f6149f8f2290f3b004a564facc7a06cb7470
Ikenna Nwodo <nnaclin@yahoo.com>
2022-09-19 15:32:47 -0700

    Task 2 ptrs and arr

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef main_h
 #define main_h
+#include <stdlib.h>
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -12,6 +13,8 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-int _putchar(char); 
+int _putchar(char c);
+size_t _strlen(const char *str);
+char *_strcpy(char *dest, char *src);
 
 #endif
