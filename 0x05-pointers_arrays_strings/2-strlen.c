e0bc8a9d81adf12e6abe8339a7b911adfd4f084b
Ikenna Nwodo <nnaclin@yahoo.com>
2022-09-19 14:30:39 -0700

    ptrs and arr

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-   * _strlen - Returns the length of a string.
-    * @str: string.
-     *
-      * Return: Length.
-       */
+ * _strlen - Returns the length of a string.
+ * @str: string.
+ *
+ * Return: Length.
+ */
 size_t _strlen(const char *str)
 {
 	size_t len = 0;
