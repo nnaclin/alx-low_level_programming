f0f650b3996c324f906403f68d5b6440121c1076
Ikenna Nwodo <nnaclin@yahoo.com>
2022-09-30 22:32:11 -0700

    Task 0

---
+++ 0-whatsmyname.c
@@ -0,0 +1,14 @@
+include <stdio.h>
+
+/** 
+  * main - this is a function that prints its own name
+  * @agrgc: argc
+  * @argv: an array of a command listed
+  * Return: 0 for succes
+  **/
+
+int main (int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
